933bce430c8b8b0003e1941016bee02104253ca2
elisa <bascongarcia@gmail.com>
2022-08-18 19:46:17 +0200

    Iteration 1 Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { Routes, Route} from 'react-router-dom';
 import Home from './views/Home';
+import Contact from './views/Contact';
+import ErrorPage from './views/ErrorPage';
+import Projects from './views/Projects';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/projects" element={<Projects />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
     </div>
   );
 }
