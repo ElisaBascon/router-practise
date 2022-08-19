0256741cd21051645f0dbb8108da4108955c3f3b
elisa <bascongarcia@gmail.com>
2022-08-19 20:23:11 +0200

    Practise Done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Contact from './views/Contact';
 import ErrorPage from './views/ErrorPage';
 import Projects from './views/Projects';
 import Navbar from './components/Navbar';
+import ProjectsApps from './components/ProjectsApps';
+import ProjectsGames from './components/ProjectsGames';
+
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/projects" element={<Projects />}/>
+        <Route path="/projects" element={<Projects />}>
+          <Route path="apps" element={<ProjectsApps />}/>
+          <Route path="games" element={<ProjectsGames />}/>
+        </Route>
         <Route path="/contact" element={<Contact />}/>
       </Routes>
     </div>
